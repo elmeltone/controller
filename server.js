6245d0b09719978ebb586ed9d515e50152604a2e
MICHAEL J MELTON <meltonm83@gmail.com>
2016-11-04 13:14:41 -0700

    Logs messages from mqtt broker

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
 const mqtt = require('mqtt');
-const client = mqtt.connect('mqtt://test.mosquitto.org');
+const client = mqtt.connect('mqtt://192.168.1.225:1883');
 
 app.use(express.static(__dirname + '/'));
 
 client.on('connect', function () {
+  client.subscribe('mqtt')
   console.log('Hello mqtt');
+  client.publish('presence', 'Hello mqtt');
 })
 
-/*client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})*/
+client.on('message', function (topic, message) {
+  console.log(message.toString());
+  /*client.end()*/
+});
 
 app.listen(process.env.PORT || 8080);
